a0b05c015fb0aef280793178b760209bf397d980
John Shaffer <jdsha@pm.me>
2026-07-08 11:42:13 -0500

    Update deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "sand development environments";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     clj-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:jlesquembre/clj-nix";
